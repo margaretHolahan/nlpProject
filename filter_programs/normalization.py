929f40e55120c07660d6bd16b37c92e08bd5949a
Margaret Holahan <69940860+margaretHolahan@users.noreply.github.com>
2022-11-30 15:56:00 -0500

    normalized one star

--- filter_programs/normalization.py
+++ filter_programs/normalization.py
@@ -26,7 +26,7 @@ def normalize(file, newfile):
         lowercase =[]
         for r in review:
             alltokens = nltk.word_tokenize(r['text'])
-            outfile = open("../json_data/first_10k/train/" + newfile + ".json", "w")
+            outfile = open("../json_data/top3/top3_user_1/" + newfile + ".json", "w")
             for word in alltokens:
                 lowercase.append(word.lower())
             for w in lowercase:
@@ -36,5 +36,5 @@ def normalize(file, newfile):
         fdist = nltk.FreqDist(thesewords)
         print(fdist.most_common(10))
 #normalize("../json_data/first_10k/test/first_10k_1_star_test.json", "normalize1")
-#normalize("../json_data/first_10k/train/first_10k_1_star.json", "normalized_1_star")
+normalize("../json_data/top3/top3_user_1/user_1_star_1.json", "normalized_1_star")
 
