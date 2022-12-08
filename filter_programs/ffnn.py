c41dcf8c89e1114230bcaa1c8534e37e51da56e3
Margaret Holahan <69940860+margaretHolahan@users.noreply.github.com>
2022-12-08 17:38:29 -0500

    ffnn

--- filter_programs/ffnn.py
+++ filter_programs/ffnn.py
@@ -1,14 +1,159 @@
+import matplotlib.pyplot as plt 
+from gensim.utils import simple_preprocess
 from gensim.parsing.porter import PorterStemmer
-from gensim import corpora
+from sklearn.model_selection import train_test_split
 
-def stem(data):
-  porter_stemmer = PorterStemmer()
-  data = [[porter_stemmer.stem(word) for word in tokens] for tokens in data]
-  return data
-X_train = stem(get_data("train", True))
-X_test = get_data("test", False)
-Y_train = get_data("train", False)
-Y_test = stem(data = get_data("test", True))
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from gensim import corpora
+import torch
+from nltk.corpus import stopwords
+import glob
+import json
+import re
+from nltk.tokenize import sent_tokenize, word_tokenize
+from gensim.models import Word2Vec
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('/content/drive/MyDrive/nlpProject/all_reviews.csv')
+shuffled_df = df.sample(frac =1)
+shuffled_df.to_csv("shuffled_data.csv", index=False)
+
+stops = stopwords.words('english')
+punct = ["'", ".", ",", '"', ";", "?", "!", "-", "-", "(", ")", "*", "/", ":", "~"]
+
+def get_data(name, answer):
+  alltext = "" 
+  toksents = []  
+  stars = []
+  all_files = glob.glob("../json_data/tokenize/first_10k/" + name + "/*")
+  for file in all_files:
+    f = open(file)
+    data = json.loads(f.read().rstrip())
+    f.close()
+    allsent = []
+    for review in data:
+      sent = review["text"]
+      allsent = sent_tokenize(sent)
+      for w in allsent:
+        if(w not in stops):
+          toksents.append(nltk.word_tokenize(w))
+          stars.append(review['stars'])
+  if(answer == True):
+    return(toksents)
+  else:
+    return(stars)
+def get_all_data(answer):
+  alltext = "" 
+  toksents = []  
+  stars = []
+  f = open("../json_data/tokenize/first_10k/all_reviews.json")
+  data = json.loads(f.read().rstrip())
+  f.close()
+  allsent = []
+  for review in data:
+    sent = review["text"]
+    allsent = sent_tokenize(sent)
+    for w in allsent:
+      if(w not in stops):
+        toksents.append(nltk.word_tokenize(w))
+        stars.append(review['stars'])
+  if(answer == True):
+    return(toksents)
+  else:
+    return(stars)
+def get_vectors(name):
+  vectors = []
+  data = get_data(name, True)
+  model = Word2Vec(data, window=5, min_count=3, workers=4)
+  for d in data:
+    totvec = np.zeros(100)
+    for w in d:
+      if w.lower() in model:
+        totvec = totvec + model[w.lower()]
+    vectors.append(totvec)
+  return vectors
+
+print(get_vectors("train")[0])
+top_data_df = pd.read_csv("shuffled_data.csv")
+
+print("Columns in the original dataset:\n")
+print(top_data_df.columns)
+
+print("Number of rows per star rating:")
+print(top_data_df['stars'].value_counts())
+
+# Function to map stars to sentiment
+def map_sentiment(stars_received):
+    if stars_received <= 2:
+        return -1
+    elif stars_received == 3:
+        return 0
+    else:
+        return 1
+# Mapping stars to sentiment into three categories
+top_data_df['sentiment'] = [ map_sentiment(x) for x in top_data_df['stars']]
+# Plotting the sentiment distribution
+plt.figure()
+pd.value_counts(top_data_df['sentiment']).plot.bar(title="Sentiment distribution in df")
+plt.xlabel("Sentiment")
+plt.ylabel("No. of rows in df")
+plt.show()
+
+# Function to retrieve top few number of each category
+def get_top_data(top_n = 5000):
+    top_data_df_positive = top_data_df[top_data_df['sentiment'] == 1].head(top_n)
+    top_data_df_negative = top_data_df[top_data_df['sentiment'] == -1].head(top_n)
+    top_data_df_neutral = top_data_df[top_data_df['sentiment'] == 0].head(top_n)
+    top_data_df_small = pd.concat([top_data_df_positive, top_data_df_negative, top_data_df_neutral])
+    return top_data_df_small
+
+# Function call to get the top 10000 from each sentiment
+top_data_df_small = get_top_data(top_n=10000)
+
+# After selecting top few samples of each sentiment
+print("After segregating and taking equal number of rows for each sentiment:")
+print(top_data_df_small['sentiment'].value_counts())
+top_data_df_small.head(10)
+
+# Tokenize the text column to get the new column 'tokenized_text'
+top_data_df_small['tokenized_text'] = [simple_preprocess(line, deacc=True) for line in top_data_df_small['text']] 
+print(top_data_df_small['tokenized_text'].head(10))
+porter_stemmer = PorterStemmer()
+# Get the stemmed_tokens
+top_data_df_small['stemmed_tokens'] = [[porter_stemmer.stem(word) for word in tokens] for tokens in top_data_df_small['tokenized_text'] ]
+top_data_df_small['stemmed_tokens'].head(10)
+# Train Test Split Function
+def split_train_test(top_data_df_small, test_size=0.3, shuffle_state=True):
+    X_train, X_test, Y_train, Y_test = train_test_split(top_data_df_small[['business_id', 'cool', 'date', 'funny', 'review_id', 'stars', 'text', 'useful', 'user_id', 'stemmed_tokens']], 
+                                                        top_data_df_small['sentiment'], 
+                                                        shuffle=shuffle_state,
+                                                        test_size=test_size, 
+                                                        random_state=15)
+    print("Value counts for Train sentiments")
+    print(Y_train.value_counts())
+    print("Value counts for Test sentiments")
+    print(Y_test.value_counts())
+    print(type(X_train))
+    print(type(Y_train))
+    X_train = X_train.reset_index()
+    X_test = X_test.reset_index()
+    Y_train = Y_train.to_frame()
+    Y_train = Y_train.reset_index()
+    Y_test = Y_test.to_frame()
+    Y_test = Y_test.reset_index()
+    print(X_train.head())
+    return X_train, X_test, Y_train, Y_test
+
+# Call the train_test_split
+X_train, X_test, Y_train, Y_test = split_train_test(top_data_df_small)
+
+# Use cuda if present
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Device available for running: ")
+print(device)
 class FeedforwardNeuralNetModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(FeedforwardNeuralNetModel, self).__init__()
@@ -41,23 +186,25 @@ class FeedforwardNeuralNetModel(nn.Module):
         out = self.fc3(out)
 
         return F.softmax(out, dim=1)
+
 # Function to return the dictionary either with padding word or without padding
-def make_dict(data, padding=True):
+def make_dict(top_data_df_small, padding=True):
     if padding:
         print("Dictionary with padded token added")
         review_dict = corpora.Dictionary([['pad']])
-        review_dict.add_documents(data)
+        review_dict.add_documents(top_data_df_small['stemmed_tokens'])
     else:
         print("Dictionary without padding")
-        review_dict = corpora.Dictionary(data)
+        review_dict = corpora.Dictionary(top_data_df_small['stemmed_tokens'])
     return review_dict
 
 # Make the dictionary without padding for the basic models
-review_dict = make_dict(stem(get_all_data(True)), padding=False)
-print(review_dict[0])
-print(len(review_dict))
+review_dict = make_dict(top_data_df_small, padding=False)
+
 VOCAB_SIZE = len(review_dict)
 NUM_LABELS = 3
+
+# Function to make bow vector to be used as input to network
 def make_bow_vector(review_dict, sentence):
     vec = torch.zeros(VOCAB_SIZE, dtype=torch.float64, device=device)
     for word in sentence:
@@ -83,7 +230,8 @@ ff_nn_bow_model.to(device)
 loss_function = nn.CrossEntropyLoss()
 optimizer = optim.SGD(ff_nn_bow_model.parameters(), lr=0.001)
 
-ffnn_loss_file_name = "loss.json"
+# Open the file for writing loss
+ffnn_loss_file_name =  "../json_data/" + 'loss.json'
 f = open(ffnn_loss_file_name,'w')
 f.write('iter, loss')
 f.write('\n')
@@ -94,18 +242,18 @@ for epoch in range(num_epochs):
     if (epoch+1) % 25 == 0:
         print("Epoch completed: " + str(epoch+1))
     train_loss = 0
-    for index in X_train:
+    for index, row in X_train.iterrows():
         # Clearing the accumulated gradients
         optimizer.zero_grad()
 
         # Make the bag of words vector for stemmed tokens 
-        bow_vec = make_bow_vector(review_dict, get_all_data(True)[index])
+        bow_vec = make_bow_vector(review_dict, row['stemmed_tokens'])
        
         # Forward pass to get output
         probs = ff_nn_bow_model(bow_vec)
 
         # Get the target label
-        target = make_target(Y_train[index])
+        target = make_target(Y_train['sentiment'][index])
 
         # Calculate Loss: softmax --> cross entropy loss
         loss = loss_function(probs, target)
@@ -121,20 +269,21 @@ for epoch in range(num_epochs):
     f.write('\n')
     train_loss = 0
 
+f.close()
+
 from sklearn.metrics import classification_report
 bow_ff_nn_predictions = []
 original_lables_ff_bow = []
 with torch.no_grad():
     for index, row in X_test.iterrows():
-        bow_vec = make_bow_vector(review_dict, get_all_data(True)[index])
+        bow_vec = make_bow_vector(review_dict, row['stemmed_tokens'])
         probs = ff_nn_bow_model(bow_vec)
         bow_ff_nn_predictions.append(torch.argmax(probs, dim=1).cpu().numpy()[0])
-        original_lables_ff_bow.append(make_target(Y_test[index]).cpu().numpy()[0])
+        original_lables_ff_bow.append(make_target(Y_test['sentiment'][index]).cpu().numpy()[0])
 print(classification_report(original_lables_ff_bow,bow_ff_nn_predictions))
 ffnn_loss_df = pd.read_csv(ffnn_loss_file_name)
 print(len(ffnn_loss_df))
 print(ffnn_loss_df.columns)
 ffnn_plt_500_padding_100_epochs = ffnn_loss_df[' loss'].plot()
 fig = ffnn_plt_500_padding_100_epochs.get_figure()
-fig.savefig("loss.json")
-f.close()
+fig.savefig( "../json_data/"+ 'plots/' + "ffnn_bow_loss_500_padding_100_epochs_less_lr.pdf")
