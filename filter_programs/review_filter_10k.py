c91b10ea1fb926ec79834c1f133a38f35afda7db
Sara Ohr <ohrsa@bc.edu>
2022-12-10 14:30:28 -0500

    Changed to 90/10 Split

--- filter_programs/review_filter_10k.py
+++ filter_programs/review_filter_10k.py
@@ -1,13 +1,14 @@
 # Program to filter out first 10k reviews
 # Filter out first 2000 reviews for each star
 import json
+import random
 
 def add_to_files(file_path, array):
     with open(file_path, "w") as fd:
         json.dump(array, fd, indent=2)
     fd.close()
 
-def main():
+def filter_stars():
     total_reviews = 0
 
     one_star = []
@@ -25,10 +26,6 @@ def main():
     restaurant_fd =  open("./json_data/restaurant_ids.json", "r")
     restaurant_ids = json.dumps(json.load(restaurant_fd))
     restaurant_fd.close()
-
-    users_fd =  open("./json_data/top3_user_ids.json", "r")
-    user_ids = json.dumps(json.load(users_fd))
-    users_fd.close()
 
     reviews_fd = open("./yelp_academic_dataset_review.json", "r")
 
@@ -59,34 +56,41 @@ def main():
                 break
     reviews_fd.close()
 
-    # 80% for Training 
-    add_to_files("./json_data/first_10k/train/first_10k_1_star.json", one_star[400:])
-    add_to_files("./json_data/first_10k/train/first_10k_2_star.json", two_star[400:])
-    add_to_files("./json_data/first_10k/train/first_10k_3_star.json", three_star[400:])
-    add_to_files("./json_data/first_10k/train/first_10k_4_star.json", four_star[400:])
-    add_to_files("./json_data/first_10k/train/first_10k_5_star.json", five_star[400:])
+    # Shuffle Reviews before splitting to testing and training sets
+    random.shuffle(one_star)
+    random.shuffle(two_star)
+    random.shuffle(three_star)
+    random.shuffle(four_star)
+    random.shuffle(five_star)
 
-    add_to_files("./json_data/first_10k/train/first_10k_review_ids_1_star_test.json", one_star_ids[:400])
-    add_to_files("./json_data/first_10k/train/first_10k_review_ids_2_star_test.json", two_star_ids[:400])
-    add_to_files("./json_data/first_10k/train/first_10k_review_ids_3_star_test.json", three_star_ids[:400])
-    add_to_files("./json_data/first_10k/train/first_10k_review_ids_4_star_test.json", four_star_ids[:400])
-    add_to_files("./json_data/first_10k/train/first_10k_review_ids_5_star_test.json", five_star_ids[:400])
+    # 90% for Training 
+    add_to_files("./json_data/first_10k/train/first_10k_1_star.json", one_star[200:])
+    add_to_files("./json_data/first_10k/train/first_10k_2_star.json", two_star[200:])
+    add_to_files("./json_data/first_10k/train/first_10k_3_star.json", three_star[200:])
+    add_to_files("./json_data/first_10k/train/first_10k_4_star.json", four_star[200:])
+    add_to_files("./json_data/first_10k/train/first_10k_5_star.json", five_star[200:])
 
-    # 20% for Testing
-    add_to_files("./json_data/first_10k/test/first_10k_1_star_test.json", one_star[:400])
-    add_to_files("./json_data/first_10k/test/first_10k_2_star_test.json", two_star[:400])
-    add_to_files("./json_data/first_10k/test/first_10k_3_star_test.json", three_star[:400])
-    add_to_files("./json_data/first_10k/test/first_10k_4_star_test.json", four_star[:400])
-    add_to_files("./json_data/first_10k/test/first_10k_5_star_test.json", five_star[:400])
+    add_to_files("./json_data/first_10k/train/first_10k_review_ids_1_star_test.json", one_star_ids[200:])
+    add_to_files("./json_data/first_10k/train/first_10k_review_ids_2_star_test.json", two_star_ids[200:])
+    add_to_files("./json_data/first_10k/train/first_10k_review_ids_3_star_test.json", three_star_ids[200:])
+    add_to_files("./json_data/first_10k/train/first_10k_review_ids_4_star_test.json", four_star_ids[200:])
+    add_to_files("./json_data/first_10k/train/first_10k_review_ids_5_star_test.json", five_star_ids[200:])
 
-    add_to_files("./json_data/first_10k/test/first_10k_review_ids_1_star.json", one_star_ids[400:])
-    add_to_files("./json_data/first_10k/test/first_10k_review_ids_2_star.json", two_star_ids[400:])
-    add_to_files("./json_data/first_10k/test/first_10k_review_ids_3_star.json", three_star_ids[400:])
-    add_to_files("./json_data/first_10k/test/first_10k_review_ids_4_star.json", four_star_ids[400:])
-    add_to_files("./json_data/first_10k/test/first_10k_review_ids_5_star.json", five_star_ids[400:])
+    # 10% for Testing
+    add_to_files("./json_data/first_10k/test/first_10k_1_star_test.json", one_star[:200])
+    add_to_files("./json_data/first_10k/test/first_10k_2_star_test.json", two_star[:200])
+    add_to_files("./json_data/first_10k/test/first_10k_3_star_test.json", three_star[:200])
+    add_to_files("./json_data/first_10k/test/first_10k_4_star_test.json", four_star[:200])
+    add_to_files("./json_data/first_10k/test/first_10k_5_star_test.json", five_star[:200])
+
+    add_to_files("./json_data/first_10k/test/first_10k_review_ids_1_star.json", one_star_ids[:200])
+    add_to_files("./json_data/first_10k/test/first_10k_review_ids_2_star.json", two_star_ids[:200])
+    add_to_files("./json_data/first_10k/test/first_10k_review_ids_3_star.json", three_star_ids[:200])
+    add_to_files("./json_data/first_10k/test/first_10k_review_ids_4_star.json", four_star_ids[:200])
+    add_to_files("./json_data/first_10k/test/first_10k_review_ids_5_star.json", five_star_ids[:200])
 
 
     print(total_reviews)
     print(len(one_star_ids), len(two_star_ids), len(three_star_ids), len(four_star_ids), len(five_star_ids))
 
-main()
+filter_stars()
